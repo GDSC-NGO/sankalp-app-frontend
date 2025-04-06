2bf8ec1e88e13b1f6dc640acf8910eba609d3910
bereck-work <bereckobrian@gmail.com>
2025-04-06 21:19:03 +0530

    (Fix): Add sqflite ffi to create sqlite database on initialization.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,24 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'pages/homepage.dart';
-import 'pages/registration.dart';
-import 'pages/login_page.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+
 import 'backend/services/auth_service.dart';
+import 'pages/homepage.dart';
+import 'pages/login_page.dart';
+import 'pages/registration.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize SQLite for desktop platforms
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    // Initialize FFI
+    sqfliteFfiInit();
+    // Change the default factory for desktop platforms
+    databaseFactory = databaseFactoryFfi;
+  }
 
   // Check if the user is logged in
   final authService = AuthService();
@@ -28,7 +40,11 @@ class MyApp extends StatelessWidget {
   final bool isFirstLaunch;
   final bool isLoggedIn;
 
-  const MyApp({super.key, required this.isFirstLaunch, required this.isLoggedIn});
+  const MyApp({
+    super.key,
+    required this.isFirstLaunch,
+    required this.isLoggedIn,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -40,9 +56,7 @@ class MyApp extends StatelessWidget {
         primaryColor: const Color(0xFF39AC9E),
       ),
       // If logged in, go to home; otherwise start with login
-      home: isLoggedIn
-          ? const MyHomePage(title: 'Sankalp')
-          : const LoginPage(),
+      home: isLoggedIn ? const MyHomePage(title: 'Sankalp') : const LoginPage(),
       routes: {
         '/home': (context) => const MyHomePage(title: 'Sankalp'),
         '/login': (context) => const LoginPage(),
